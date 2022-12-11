e8be098bea71d7d706fe9dc6cb94c2265bb7dd76
Noxville <bensteenhuisen@gmail.com>
2022-12-11 06:46:06 +0100

    Day 11: Not too much throwing by me.

---
+++ 11/main2.py
@@ -0,0 +1,23 @@
+from functools import reduce
+from main1 import Monkey
+
+if __name__ == "__main__":
+    monkeys = {}
+    with open('case1.in') as fin:
+        inp = [e.strip() for e in fin.readlines()]
+        for midx in range((len(inp) + 1) // 7):
+            monkeys[midx] = Monkey(midx, inp[7 * midx:(7 * (midx + 1) - 1)])
+        lcm = reduce((lambda a, b: a * b), [m.div for m in monkeys.values()])
+        for rnd in range(10000):
+            for midx in sorted(monkeys.keys()):
+                m = monkeys[midx]
+                while m.inv:
+                    item = m.inspects()
+                    _op = str(m.op).replace('old', str(item))
+                    worry = eval(_op) % lcm
+                    if worry % m.div == 0:
+                        monkeys[m.tr].add_item(worry)
+                    else:
+                        monkeys[m.fa].add_item(worry)
+    std = sorted([m.inspections for m in monkeys.values()])
+    print(std[-1] * std[-2])
